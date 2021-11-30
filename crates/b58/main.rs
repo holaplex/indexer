a95d4ff7ae45c0e4fad681dcb63317df5492b57e
raykast <rookie1286@gmail.com>
2021-11-30 12:13:21 -0800

    Convenience additions to the b58 utility.

--- crates/b58/main.rs
+++ crates/b58/main.rs
@@ -1,7 +1,24 @@
-use std::io::{prelude::*, stdin, stdout};
+use std::{
+    borrow::Cow,
+    io::{prelude::*, stdin, stdout},
+};
 
 use bs58::{decode, encode};
 use clap::Parser;
+
+#[derive(Parser)]
+struct Opts {
+    /// Disable stripping a final `\n` off of STDIN
+    #[clap(long, short = 'S')]
+    no_strip: bool,
+
+    /// Use hexadecimal instead of raw binary
+    #[clap(long, short = 'x')]
+    hex: bool,
+
+    #[clap(subcommand)]
+    command: Command,
+}
 
 #[derive(Parser)]
 enum Command {
@@ -10,20 +27,50 @@ enum Command {
 }
 
 fn main() {
-    let cmd = Command::parse();
+    let Opts {
+        no_strip,
+        hex,
+        command,
+    } = Opts::parse();
 
     let mut contents = Vec::new();
     stdin().read_to_end(&mut contents).unwrap();
 
-    match cmd {
+    let contents = if no_strip {
+        &contents
+    } else {
+        std::str::from_utf8(&contents)
+            .ok()
+            .and_then(|s| s.rsplit_once('\n').map(|(l, _)| l.as_bytes()))
+            .unwrap_or(&contents)
+    };
+
+    match command {
         Command::Encode => {
             stdout()
-                .write_all(encode(contents).into_string().as_bytes())
+                .write_all(
+                    encode(if hex {
+                        Cow::Owned(hex::decode(contents).unwrap())
+                    } else {
+                        Cow::Borrowed(contents)
+                    })
+                    .into_string()
+                    .as_bytes(),
+                )
                 .unwrap();
         },
         Command::Decode => {
+            let bin = &decode(contents).into_vec().unwrap();
+
             stdout()
-                .write_all(&decode(contents).into_vec().unwrap())
+                .write_all(
+                    if hex {
+                        Cow::Owned(hex::encode(bin).into_bytes())
+                    } else {
+                        Cow::Borrowed(bin)
+                    }
+                    .as_ref(),
+                )
                 .unwrap();
         },
     }
