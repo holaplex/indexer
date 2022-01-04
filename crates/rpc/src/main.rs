0a0093e6e911454abcc5a0c8f39aff97938b75df
raykast <rookie1286@gmail.com>
2022-01-04 13:29:41 -0800

    Add required /v<VERSION> prefix to all routes.

--- crates/rpc/src/main.rs
+++ crates/rpc/src/main.rs
@@ -45,9 +45,64 @@ fn main() {
         addr.set_port(port);
         info!("Listening on {}", addr);
 
-        let server = ServerBuilder::new(io)
-            .start_http(&addr)
-            .context("Failed to start RPC server")?;
+        let server = {
+            use jsonrpc_http_server::hyper;
+
+            let version_prefix = regex::Regex::new(&format!(
+                r"^/?v{}/?(.*)",
+                regex::escape(
+                    &percent_encoding::utf8_percent_encode(
+                        env!("CARGO_PKG_VERSION_MAJOR"),
+                        percent_encoding::NON_ALPHANUMERIC
+                    )
+                    .to_string()
+                )
+            ))
+            .unwrap();
+
+            ServerBuilder::new(io)
+                .request_middleware(move |mut r: hyper::Request<_>| {
+                    match version_prefix.captures(r.uri().path()) {
+                        Some(caps) => {
+                            let mut b = hyper::Uri::builder();
+                            let hyper::http::uri::Parts {
+                                scheme,
+                                authority,
+                                path_and_query,
+                                ..
+                            } = r.uri().clone().into_parts();
+
+                            if let Some(scheme) = scheme {
+                                b = b.scheme(scheme);
+                            }
+
+                            if let Some(authority) = authority {
+                                b = b.authority(authority);
+                            }
+
+                            let mut path = format!("/{}", &caps[1]);
+
+                            if let Some(query) = path_and_query.unwrap().query() {
+                                path = format!("{}?{}", path, query);
+                            }
+
+                            *r.uri_mut() = b.path_and_query(path).build().unwrap();
+
+                            r.into()
+                        },
+                        None => hyper::Response::builder()
+                            .status(hyper::StatusCode::BAD_REQUEST)
+                            .body(
+                                format!("API version mismatch, expected {:?}\n", version_prefix)
+                                    .into(),
+                            )
+                            .unwrap()
+                            .into(),
+                    }
+                })
+                .start_http(&addr)
+                .context("Failed to start RPC server")?
+        };
 
         server.wait();
 
