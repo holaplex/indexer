5ae7256a2008a7646410f7510fc9193f2c91fd54
Ryan S <6175424+ray-kast@users.noreply.github.com>
2022-01-10 09:34:18 -0800

    Add proper lint config for graphql crate

--- crates/graph/src/main.rs
+++ crates/graph/src/main.rs
@@ -1,6 +1,12 @@
-//! Actix web juniper example
-//!
-//! A simple example integrating juniper in actix-web
+//! GraphQL server to read from `metaplex-indexer`
+
+#![deny(
+    clippy::suspicious,
+    clippy::style,
+    missing_debug_implementations,
+    missing_copy_implementations
+)]
+#![warn(clippy::pedantic, clippy::cargo, missing_docs)]
 use std::io;
 use std::sync::Arc;
 
