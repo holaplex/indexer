0d5d50971f54ca860221c40bc0bd2171780cbabf
raykast <rookie1286@gmail.com>
2022-01-14 17:07:57 -0800

    [scaffolding intensifies]

--- crates/accountsdb-rabbitmq/src/plugin.rs
+++ crates/accountsdb-rabbitmq/src/plugin.rs
@@ -1,14 +1,38 @@
-use crate::interface::{AccountsDbPlugin, ReplicaAccountInfoVersions, Result};
+use indexer_rabbitmq::{
+    accountsdb::{Message, Producer},
+    lapin::{Connection, ConnectionProperties},
+};
+use lapinou::LapinSmolExt;
+
+use crate::interface::{
+    AccountsDbPlugin, AccountsDbPluginError, ReplicaAccountInfoVersions, Result,
+};
+
+fn custom_err(e: impl std::error::Error + Send + Sync + 'static) -> AccountsDbPluginError {
+    AccountsDbPluginError::Custom(Box::new(e))
+}
 
 /// An instance of the plugin
 #[derive(Debug, Default)]
 pub struct AccountsDbPluginRabbitMq {
-    todo: Vec<()>, // TODO
+    producer: Option<Producer>,
 }
 
 impl AccountsDbPlugin for AccountsDbPluginRabbitMq {
     fn name(&self) -> &'static str {
         "AccountsDbPluginRabbitMq"
+    }
+
+    fn on_load(&mut self, _cfg: &str) -> Result<()> {
+        smol::block_on(async {
+            let conn = Connection::connect("todo", ConnectionProperties::default().with_smol())
+                .await
+                .map_err(custom_err)?;
+
+            self.producer = Some(Producer::new(&conn).await.map_err(custom_err)?);
+
+            Ok(())
+        })
     }
 
     fn update_account(
@@ -17,6 +41,29 @@ impl AccountsDbPlugin for AccountsDbPluginRabbitMq {
         slot: u64,
         is_startup: bool,
     ) -> Result<()> {
-        Ok(())
+        fn uninit() -> AccountsDbPluginError {
+            AccountsDbPluginError::AccountsUpdateError {
+                msg: "RabbitMQ plugin not initialized yet!".into(),
+            }
+        }
+
+        smol::block_on(async {
+            match account {
+                ReplicaAccountInfoVersions::V0_0_1(acct) => {
+                    self.producer
+                        .as_ref()
+                        .ok_or_else(uninit)?
+                        .write(Message::AccountUpdate {
+                            key: acct.pubkey.try_into().map_err(custom_err)?,
+                            owner: acct.owner.try_into().map_err(custom_err)?,
+                            data: acct.data.to_owned(),
+                        })
+                        .await
+                        .map_err(custom_err)?;
+                },
+            }
+
+            Ok(())
+        })
     }
 }
---
+++ crates/rabbitmq/src/accountsdb.rs
@@ -0,0 +1,63 @@
+use lapin::{
+    options::{BasicPublishOptions, ExchangeDeclareOptions},
+    types::FieldTable,
+    BasicProperties, Channel, ExchangeKind,
+};
+use serde::{Deserialize, Serialize};
+
+use crate::Result;
+
+pub type Pubkey = [u8; 32];
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Message {
+    AccountUpdate {
+        key: Pubkey,
+        owner: Pubkey,
+        data: Vec<u8>,
+    },
+    InstructionNotify {
+        program: Pubkey,
+        data: Vec<u8>,
+    },
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct QueueType;
+
+#[async_trait::async_trait]
+impl crate::QueueType<Message> for QueueType {
+    const EXCHANGE: &'static str = "accounts";
+    const QUEUE: &'static str = "";
+
+    async fn init_producer(chan: &Channel) -> Result<()> {
+        chan.exchange_declare(
+            "accounts",
+            ExchangeKind::Fanout,
+            ExchangeDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
+        Ok(())
+    }
+
+    async fn init_consumer(chan: &Channel) -> Result<()> {
+        todo!();
+
+        Ok(())
+    }
+
+    fn publish_opts(_: &Message) -> BasicPublishOptions {
+        BasicPublishOptions::default()
+    }
+
+    fn properties(_: &Message) -> BasicProperties {
+        BasicProperties::default()
+    }
+}
+
+#[cfg(feature = "producer")]
+pub type Producer = crate::producer::Producer<Message, QueueType>;
+#[cfg(feature = "consumer")]
+pub type Consumer = crate::consumer::Consumer<Message, QueueType>;
