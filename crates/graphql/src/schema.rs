4dd3f840acd9b87ecb0f70b1d080fb018470c9ae
Jason Schorr <echohtp@gmail.com>
2022-01-20 17:21:27 -0400

    it works!

--- crates/graphql/src/schema.rs
+++ crates/graphql/src/schema.rs
@@ -68,7 +68,7 @@ impl QueryRoot {
     fn nfts(
         &self,
         #[graphql(description = "Filter on creator address")] creators: Option<Vec<String>>,
-        #[graphql(description = "Filter on update authority addres")] _update_authority: Option<
+        #[graphql(description = "Filter on update authority addres")] update_authority: Option<
             Vec<String>,
         >,
     ) -> Vec<Nft> {
@@ -100,8 +100,7 @@ impl QueryRoot {
                     .unwrap();
 
                 // get the nft from the hashmap or create a new object
-                let creators_vec: Vec<String> = Vec::new();
-                let nft = nfts_hash.entry(address.clone()).or_insert(Nft {
+                nfts_hash.entry(address.clone()).or_insert(Nft {
                     address: token[0].address.to_string(),
                     name: token[0].name.to_string(),
                     symbol: token[0].symbol.to_string(),
@@ -111,43 +110,59 @@ impl QueryRoot {
                     mint_address: token[0].mint_address.to_string(),
                     primary_sale_happened: token[0].primary_sale_happened,
                     is_mutable: token[0].is_mutable,
-                    creators: creators_vec,
+                    creators: vec![]
                 });
-
-                // push the creator to the creators vector
-                nft.creators.push(creator.clone());
             }
         }
 
-        let all_nfts: Vec<Nft> = nfts_hash.into_iter().map(|(_id, v)| v).collect();
+        // for every update authority in ua parameter
+        for ua in update_authority.into_iter().flatten().collect::<Vec<String>>() {
+            // get all token addresses by creator
+            let tokens: Vec<String> = metadata_creators::table
+                .select(metadata_creators::metadata_address)
+                .filter(metadata_creators::creator_address.eq(&ua))
+                .load(&conn)
+                .unwrap();
+
+            // get nft from token address
+            for address in &tokens {
+                // get the token metadata
+                let token: Vec<models::Metadata> = metadatas::table
+                    .select(metadatas::all_columns)
+                    .filter(metadatas::address.eq(address))
+                    .limit(1)
+                    .load(&conn)
+                    .unwrap();
+
+                // get the nft from the hashmap or create a new object
+                nfts_hash.entry(address.clone()).or_insert(Nft {
+                    address: token[0].address.to_string(),
+                    name: token[0].name.to_string(),
+                    symbol: token[0].symbol.to_string(),
+                    uri: token[0].uri.to_string(),
+                    seller_fee_basis_points: token[0].seller_fee_basis_points,
+                    update_authority_address: ua.to_owned(),
+                    mint_address: token[0].mint_address.to_string(),
+                    primary_sale_happened: token[0].primary_sale_happened,
+                    is_mutable: token[0].is_mutable,
+                    creators: vec![]
+                });
+            }
+        }
+        
+        let mut all_nfts: Vec<Nft> = Vec::new();
+        for (_k, mut nft) in nfts_hash.into_iter(){
+            let mut creators: Vec<String> = metadata_creators::table
+                .select(metadata_creators::creator_address)
+                .filter(metadata_creators::metadata_address.eq(&nft.address))
+                .load(&conn)
+                .unwrap();
+            
+                nft.creators.append(&mut creators);
+                all_nfts.push(nft);
+        }
+
         all_nfts
-        // // Find all creators
-        // let creator_list: Vec<String> = creators.into_iter().flatten().collect();
-        // let mut rows: Vec<String> = metadata_creators::table
-        //     .select(metadata_creators::metadata_address)
-        //     .filter(metadata_creators::creator_address.eq(any(creator_list)))
-        //     .load(&conn)
-        //     .unwrap();
-        // all_rows.append(&mut rows);
-
-        // // Find all update authorities
-        // let update_authority_list: Vec<String> = update_authority.into_iter().flatten().collect();
-        // let mut rows: Vec<String> = metadatas::table
-        //     .select(metadatas::address)
-        //     .filter(metadatas::update_authority_address.eq(any(update_authority_list)))
-        //     .load(&conn)
-        //     .unwrap();
-        // all_rows.append(&mut rows);
-
-        // // Find all NFTs
-        // let rows: Vec<models::Metadata> = metadatas::table
-        //     .select(metadatas::all_columns)
-        //     .filter(metadatas::address.eq(any(all_rows)))
-        //     .load(&conn)
-        //     .unwrap();
-
-        // // Convert the models::Metadata ➡️ stuct
-        // // rows.into_iter().map(Into::into).collect()
     }
 
     fn nft(&self, #[graphql(description = "Address of NFT")] address: String) -> Nft {
@@ -160,7 +175,6 @@ impl QueryRoot {
             .unwrap();
 
         // rows.pop().map(Into::into)
-        let creators_vec: Vec<String> = Vec::new();
         Nft {
             address: rows[0].address.to_string(),
             name: rows[0].name.to_string(),
@@ -171,7 +185,7 @@ impl QueryRoot {
             mint_address: rows[0].mint_address.to_string(),
             primary_sale_happened: rows[0].primary_sale_happened,
             is_mutable: rows[0].is_mutable,
-            creators: creators_vec,
+            creators: vec![],
         }
     }
 }
@@ -180,7 +194,6 @@ pub struct MutationRoot;
 #[juniper::graphql_object]
 impl MutationRoot {
     fn buyNft(_buy_nft: BuyNft) -> FieldResult<Nft> {
-        let creator_vec: Vec<String> = Vec::new();
         Ok(Nft {
             address: "abc123".to_owned(),
             name: "foo".to_owned(),
@@ -191,7 +204,7 @@ impl MutationRoot {
             mint_address: "efg890".to_owned(),
             primary_sale_happened: false,
             is_mutable: true,
-            creators: creator_vec,
+            creators: vec![],
         })
     }
 }
