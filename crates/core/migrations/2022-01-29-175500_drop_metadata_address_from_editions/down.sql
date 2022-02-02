d9e4a84003812b4137c128c7b8f88baf34f984f6
raykast <rookie1286@gmail.com>
2022-02-02 12:47:12 -0800

    Post-rebase chaos (graphql still won't build D:)

--- crates/core/migrations/2022-01-29-175500_drop_metadata_address_from_editions/down.sql
+++ crates/core/migrations/2022-01-29-175500_drop_metadata_address_from_editions/down.sql
@@ -1,1 +1,8 @@
--- This file should undo anything in `up.sql`
+delete from editions;
+delete from master_editions;
+
+alter table editions
+add column metadata_address varchar(48) not null;
+
+alter table master_editions
+add column metadata_address varchar(48) not null;
