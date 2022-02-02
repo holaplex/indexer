64e51a126113f080eb840614d38e769b1faf95db
raykast <rookie1286@gmail.com>
2022-02-01 16:45:36 -0800

    Store AuctionCache accounts.

--- crates/indexer/src/accountsdb/auction.rs
+++ crates/indexer/src/accountsdb/auction.rs
@@ -1,26 +1,17 @@
-use std::borrow::Borrow;
-
 use chrono::{offset::Local, Duration, NaiveDateTime};
 use indexer_core::{
     db::{
         insert_into,
-        models::{AuctionData, AuctionDataExt, Bid},
-        select,
-        tables::{auction_datas, auction_datas_ext, bids},
-        Connection,
+        models::{AuctionData, AuctionDataExt},
+        tables::{auction_datas, auction_datas_ext},
     },
     prelude::*,
-    pubkeys::find_auction_data_extended,
 };
 use metaplex_auction::processor::{
-    AuctionData as AuctionDataAccount, AuctionDataExtended, BidState, BidderMetadata, PriceFloor,
+    AuctionData as AuctionDataAccount, AuctionDataExtended, BidState, PriceFloor,
     BASE_AUCTION_DATA_SIZE,
 };
 
-use super::{
-    bidder_metadata::{BidList, BidMap},
-    RcAuctionKeys,
-};
 use crate::{prelude::*, util, Client};
 
 async fn process_auction_data(
@@ -58,10 +49,8 @@ async fn process_auction_data(
     let values = AuctionData {
         address: Owned(bs58::encode(key).into_string()),
         ends_at,
-        // created_at: todo!(), // keys.created_at,
         authority: Owned(bs58::encode(auction.authority).into_string()),
         token_mint: Owned(bs58::encode(auction.token_mint).into_string()),
-        store_owner: todo!(), // Owned(bs58::encode(keys.store_owner).into_string()),
         highest_bid,
         last_bid_time,
         // TODO: horrible abuse of the NaiveDateTime struct but Metaplex does
@@ -78,26 +67,10 @@ async fn process_auction_data(
             PriceFloor::BlindedPrice(_) => Some(-1),
         },
         total_uncancelled_bids,
-        // gap_tick_size: todo!(), // ext.gap_tick_size_percentage.map(Into::into),
-        // instant_sale_price: todo!(), [> ext
-        //                          *     .instant_sale_price
-        //                          *     .map(TryFrom::try_from)
-        //                          *     .transpose()
-        //                          *     .context("Instant sale price is too high to store")?,
-        //                          */
-        // name: todo!(), [> Borrowed(
-        //                 *     ext.name
-        //                 *         .as_ref()
-        //                 *         .map(|n| std::str::from_utf8(n))
-        //                 *         .transpose()
-        //                 *         .context("Couldn't parse auction name")?
-        //                 *         .unwrap_or("")
-        //                 *         .trim_end_matches('\0'),
-        //                 * ), */
     };
 
     client
-        .db(|db| {
+        .db(move |db| {
             insert_into(auction_datas::table)
                 .values(&values)
                 .on_conflict(auction_datas::address)
@@ -106,7 +79,7 @@ async fn process_auction_data(
                 .execute(db)
         })
         .await
-        .context("Failed to insert listing")?;
+        .context("Failed to insert AuctionData")?;
 
     Ok(())
 }
@@ -116,6 +89,38 @@ async fn process_auction_data_extended(
     key: Pubkey,
     ext: AuctionDataExtended,
 ) -> Result<()> {
+    let values = AuctionDataExt {
+        address: Owned(bs58::encode(key).into_string()),
+        gap_tick_size: ext.gap_tick_size_percentage.map(Into::into),
+        instant_sale_price: ext
+            .instant_sale_price
+            .map(TryFrom::try_from)
+            .transpose()
+            .context("Instant sale price is too high to store")?,
+        name: Owned(
+            ext.name
+                .as_ref()
+                .map(|n| std::str::from_utf8(n))
+                .transpose()
+                .context("Couldn't parse auction name")?
+                .unwrap_or("")
+                .trim_end_matches('\0')
+                .to_owned(),
+        ),
+    };
+
+    client
+        .db(move |db| {
+            insert_into(auction_datas_ext::table)
+                .values(&values)
+                .on_conflict(auction_datas_ext::address)
+                .do_update()
+                .set(&values)
+                .execute(db)
+        })
+        .await
+        .context("Failed to insert AuctionDataExtended")?;
+
     Ok(())
 }
 
@@ -152,113 +157,7 @@ pub(super) async fn process(
     }
 }
 
-//     let (ext, _bump) = find_auction_data_extended(&keys.vault);
-
-//     let mut acct = client
-//         .get_account(&ext)
-//         .context("Failed to get extended auction data")?;
-
-//     let ext = AuctionDataExtended::from_account_info(&util::account_as_info(
-//         &ext, false, false, &mut acct,
-//     ))
-//     .context("Failed to parse AuctionDataExtended")?;
-
-//     let total_uncancelled_bids;
-//     let highest_bid;
-
-//     match auction.bid_state {
-//         BidState::EnglishAuction { ref bids, .. } => {
-//             total_uncancelled_bids = Some(
-//                 bids.len()
-//                     .try_into()
-//                     .context("Bid count is too high to store!")?,
-//             );
-
-//             highest_bid = bids
-//                 .iter()
-//                 .map(|b| b.1)
-//                 .max()
-//                 .map(|h| h.try_into().context("Highest bid is too high to store!"))
-//                 .transpose()?;
-//         },
-//         BidState::OpenEdition { .. } => {
-//             total_uncancelled_bids = None;
-//             highest_bid = None;
-//         },
-//     }
-
-//     let now = Local::now().naive_utc();
-//     let (ends_at, ended, last_bid_time) = get_end_info(&auction, now)?;
-//     let auction_address = bs58::encode(keys.auction).into_string();
-
-//     let values = Listing {
-//         address: Borrowed(&auction_address),
-//         ends_at,
-//         created_at: keys.created_at,
-//         ended,
-//         authority: Owned(bs58::encode(auction.authority).into_string()),
-//         token_mint: Owned(bs58::encode(auction.token_mint).into_string()),
-//         store_owner: Owned(bs58::encode(keys.store_owner).into_string()),
-//         highest_bid,
-//         last_bid_time,
-//         // TODO: horrible abuse of the NaiveDateTime struct but Metaplex does
-//         //       roughly the same thing with the solana UnixTimestamp struct.
-//         end_auction_gap: auction
-//             .end_auction_gap
-//             .map(|g| NaiveDateTime::from_timestamp(g, 0)),
-//         price_floor: match auction.price_floor {
-//             PriceFloor::None(_) => None,
-//             PriceFloor::MinimumPrice(p) => Some(
-//                 p[0].try_into()
-//                     .context("Price floor is too high to store")?,
-//             ),
-//             PriceFloor::BlindedPrice(_) => Some(-1),
-//         },
-//         total_uncancelled_bids,
-//         gap_tick_size: ext.gap_tick_size_percentage.map(Into::into),
-//         instant_sale_price: ext
-//             .instant_sale_price
-//             .map(TryFrom::try_from)
-//             .transpose()
-//             .context("Instant sale price is too high to store")?,
-//         name: Borrowed(
-//             ext.name
-//                 .as_ref()
-//                 .map(|n| std::str::from_utf8(n))
-//                 .transpose()
-//                 .context("Couldn't parse auction name")?
-//                 .unwrap_or("")
-//                 .trim_end_matches('\0'),
-//         ),
-//     };
-
-//     let db = client.db()?;
-
-//     insert_into(listings::table)
-//         .values(&values)
-//         .on_conflict(listings::address)
-//         .do_update()
-//         .set(&values)
-//         .execute(&db)
-//         .context("Failed to insert listing")?;
-
-//     debug_assert!(!bid_map.read().await.is_empty());
-
-//     store_bids(
-//         &keys.auction,
-//         &auction_address,
-//         bid_map
-//             .read()
-//             .await
-//             .get(&keys.auction)
-//             .into_iter()
-//             .flatten(),
-//         &db,
-//     )?;
-
-//     Ok(())
-// }
-
+// TODO: handle bids
 // pub fn process_solo_bids(client: &Client, auction: Pubkey, bids: BidList) -> Result<()> {
 //     let db = client.db()?;
 //     let auction_addr = bs58::encode(auction).into_string();
