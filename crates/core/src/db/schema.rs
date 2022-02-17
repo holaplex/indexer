3fbac1bc8f71fc57ab2144a607eff6a14d554ad3
Abdul Basit <baronbasit@gmail.com>
2022-02-17 05:58:32 +0500

    schema

--- crates/core/src/db/schema.rs
+++ crates/core/src/db/schema.rs
@@ -267,7 +267,7 @@ table! {
         address -> Varchar,
         mint_address -> Varchar,
         owner_address -> Varchar,
-        amount -> Nullable<Int8>,
+        amount -> Int8,
         updated_at -> Timestamp,
     }
 }
