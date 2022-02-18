211437b42dbcc574dbb56e72230809a01eb2f7db
raykast <rookie1286@gmail.com>
2022-02-18 13:52:21 -0800

    All features.  No deps.

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -11,6 +11,6 @@ build_flags=(--workspace --lib --bins --all-features)
 diff --unified <(./diesel.sh print-schema) crates/core/src/db/schema.rs
 "$CARGO" fmt --all --check
 "$CARGO" clippy "${build_flags[@]}"
-"$CARGO" doc "${build_flags[@]}"
+"$CARGO" doc "${build_flags[@]}" --no-deps
 "$CARGO" build "${build_flags[@]}"
 "$CARGO" test "${build_flags[@]}"
