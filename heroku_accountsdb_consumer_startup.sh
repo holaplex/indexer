abe08fd2104f98ada55ca03f1e97d59fce22e527
Daniel Miller <danielmiller@Daniels-MacBook-Pro.local>
2022-02-22 20:21:32 -0800

    separate app for http consumer

--- heroku_accountsdb_consumer_startup.sh
+++ heroku_accountsdb_consumer_startup.sh
@@ -1,3 +1,1 @@
-./bin/metaplex-indexer-http --entity store-config --sender mainnet &
-./bin/metaplex-indexer-http --entity metadata-json --sender mainnet &
 ./bin/metaplex-indexer-accountsdb --network=mainnet
