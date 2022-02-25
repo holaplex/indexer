eab77de78aebb0ecde43b0d93a6ad765ec0c5372
raykast <rookie1286@gmail.com>
2022-02-25 15:58:58 -0800

    Index BidderMetadata.

--- crates/indexer/src/accountsdb/accounts/bidder_metadata.rs
+++ crates/indexer/src/accountsdb/accounts/bidder_metadata.rs
@@ -1,86 +1,44 @@
-use std::{panic::AssertUnwindSafe, sync::Arc};
+use indexer_core::db::{insert_into, models::Bid, tables::bids};
+use metaplex_auction::processor::BidderMetadata as BidderMetadataAccount;
 
-use indexer_core::{hash::HashMap, pubkeys};
-use metaplex_auction::processor::{BidderMetadata, BIDDER_METADATA_LEN};
-use tokio::sync::RwLock;
+use super::Client;
+use crate::prelude::*;
 
-use crate::{client::prelude::*, prelude::*, util, Client};
+pub(crate) async fn process(
+    client: &Client,
+    key: Pubkey,
+    meta: BidderMetadataAccount,
+) -> Result<()> {
+    let BidderMetadataAccount {
+        bidder_pubkey,
+        auction_pubkey,
+        last_bid,
+        last_bid_timestamp,
+        cancelled,
+    } = meta;
 
-pub type BidList = Vec<BidderMetadata>;
-type BidMapInner = RwLock<HashMap<Pubkey, BidList>>;
-pub struct BidMap(AssertUnwindSafe<Arc<BidMapInner>>);
+    let values = Bid {
+        listing_address: Owned(bs58::encode(auction_pubkey).into_string()),
+        bidder_address: Owned(bs58::encode(bidder_pubkey).into_string()),
+        last_bid_time: NaiveDateTime::from_timestamp(last_bid_timestamp, 0),
+        last_bid_amount: last_bid
+            .try_into()
+            .context("Last bid amount was too high to store")?,
+        cancelled,
+    };
 
-impl std::ops::Deref for BidMap {
-    type Target = BidMapInner;
-
-    fn deref(&self) -> &Self::Target {
-        &*self.0
-    }
-}
-
-impl Default for BidMap {
-    fn default() -> Self {
-        Self(AssertUnwindSafe(Arc::new(RwLock::new(HashMap::default()))))
-    }
-}
-
-impl Clone for BidMap {
-    fn clone(&self) -> Self {
-        Self(AssertUnwindSafe(Arc::clone(&*self.0)))
-    }
-}
-
-fn get_internal(client: &Client) -> Result<HashMap<Pubkey, BidList>> {
-    let mut map = HashMap::default();
-
-    let start_time = Local::now();
-
-    let res = client.get_program_accounts(pubkeys::auction(), RpcProgramAccountsConfig {
-        filters: Some(vec![RpcFilterType::DataSize(
-            BIDDER_METADATA_LEN.try_into().unwrap(),
-        )]),
-        ..RpcProgramAccountsConfig::default()
-    });
-
-    let end_time = Local::now();
-
-    info!(
-        "Bidder metadata call completed in {}",
-        indexer_core::util::duration_hhmmssfff(end_time - start_time)
-    );
-
-    res.context("Failed to retrieve bids for auction")?
-        .into_iter()
-        .filter_map(|(key, mut acct)| {
-            let parsed = BidderMetadata::from_account_info(&util::account_as_info(
-                &key, false, false, &mut acct,
-            ))
-            .map_err(|e| debug!("Failed to parse possible bidder metadata: {:?}", e))
-            .ok()?;
-
-            let (key2, _bump) =
-                pubkeys::find_bidder_metadata(parsed.auction_pubkey, parsed.bidder_pubkey);
-
-            if key != key2 {
-                debug!("Failed to assert derivation of bidder metadata PDA");
-                return None;
-            }
-
-            Some(parsed)
+    client
+        .db()
+        .run(move |db| {
+            insert_into(bids::table)
+                .values(&values)
+                .on_conflict((bids::listing_address, bids::bidder_address))
+                .do_update()
+                .set(&values)
+                .execute(db)
         })
-        .for_each(|acct| {
-            map.entry(acct.auction_pubkey)
-                .or_insert_with(Vec::new)
-                .push(acct);
-        });
-
-    Ok(map)
-}
-
-pub async fn get(client: &Client, bid_map: &BidMap) -> Result<()> {
-    get_internal(client)
-        .map(|m| async { *bid_map.write().await = m })?
-        .await;
+        .await
+        .context("Failed to store bidder metadata")?;
 
     Ok(())
 }
