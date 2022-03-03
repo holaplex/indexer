8726841767c7ed72d1ab172e79b880a48781ce50
Daniel Miller <danielmiller@Daniels-MacBook-Pro.local>
2022-03-03 12:04:48 -0800

    load filtered metadatas using left_join hack

--- crates/core/src/db/queries/metadatas.rs
+++ crates/core/src/db/queries/metadatas.rs
@@ -8,8 +8,8 @@ use diesel::{
 
 use crate::{
     db::{
-        schema,
         models::Nft,
+        schema,
         tables::{attributes, metadata_creators, metadata_jsons, metadatas, token_accounts},
         Connection, Pool,
     },
@@ -20,37 +20,6 @@ use crate::{
 pub struct MetadataFilterAttributes {
     pub trait_type: String,
     pub values: Vec<String>,
-}
-type Q = diesel::query_builder::BoxedSelectStatement<'static,
-    (diesel::sql_types::Text,
-    diesel::sql_types::Text,
-    diesel::sql_types::Text,
-    diesel::sql_types::Text,
-    diesel::sql_types::Integer,
-    diesel::sql_types::Text,
-    diesel::sql_types::Text,
-    diesel::sql_types::Bool,
-    diesel::sql_types::Bool,
-    diesel::sql_types::Nullable<diesel::sql_types::Integer>,
-    diesel::sql_types::Text),
-    schema::metadatas::table,
-    diesel::pg::Pg,
->;
-
-fn build_attributes_query(attributes: Vec<MetadataFilterAttributes>, query: Q) -> Q {
-    attributes
-        .into_iter()
-        .fold(query, |acc, MetadataFilterAttributes { trait_type, values }| {
-            let sub = attributes::table
-                .select(attributes::metadata_address)
-                .filter(
-                    attributes::trait_type
-                        .eq(trait_type)
-                        .and(attributes::value.eq(any(values))),
-                );
-
-            acc.filter(metadatas::address.eq(any(sub)))
-        })
 }
 
 /// Handles queries for NFTs
@@ -63,64 +32,61 @@ pub fn load_filtered(
     creators: Option<Vec<String>>,
     attributes: Option<Vec<MetadataFilterAttributes>>,
 ) -> Result<Vec<Nft>> {
+    let mut query = metadatas::table
+        .left_join(
+            metadata_creators::table.on(metadatas::address.eq(metadata_creators::metadata_address)),
+        )
+        .left_join(
+            metadata_jsons::table.on(metadatas::address.eq(metadata_jsons::metadata_address)),
+        )
+        .left_join(
+            token_accounts::table.on(metadatas::mint_address.eq(token_accounts::mint_address)),
+        )
+        .into_boxed();
 
-    let mut query = metadatas::table.into_boxed();
     if let Some(attributes) = attributes {
-        query = build_attributes_query(attributes, query);
+        query = attributes.into_iter().fold(
+            query,
+            |acc, MetadataFilterAttributes { trait_type, values }| {
+                let sub = attributes::table
+                    .select(attributes::metadata_address)
+                    .filter(
+                        attributes::trait_type
+                            .eq(trait_type)
+                            .and(attributes::value.eq(any(values))),
+                    );
+
+                acc.filter(metadatas::address.eq(any(sub)))
+            },
+        );
     }
 
-    let rows: Vec<Nft> = if let Some(creators) = creators {
-        let creator_q = query
-            .inner_join(
-                metadata_creators::table
-                    .on(metadatas::address.eq(metadata_creators::metadata_address)),
-            )
-            .inner_join(
-                metadata_jsons::table.on(metadatas::address.eq(metadata_jsons::metadata_address)),
-            )
-            .select((
-                metadatas::address,
-                metadatas::name,
-                metadatas::seller_fee_basis_points,
-                metadatas::mint_address,
-                metadatas::primary_sale_happened,
-                metadata_jsons::description,
-                metadata_jsons::image,
-            ))
-            .filter(metadata_creators::creator_address.eq(any(creators)))
-            .order_by(metadatas::name.desc());
-
-        let sql = debug_query::<diesel::pg::Pg, _>(&creator_q);
-        let result = sql.to_string().replace("\"", "");
-        debug!("THIS: {:?}", result);
-
-        creator_q.load(conn).context("failed to load nft(s)")?
-    } else if let Some(owners) = owners {
-        // owners
-        query
-            .inner_join(
-                token_accounts::table.on(metadatas::mint_address.eq(token_accounts::mint_address)),
-            )
-            .inner_join(
-                metadata_jsons::table.on(metadatas::address.eq(metadata_jsons::metadata_address)),
-            )
+    if let Some(creators) = creators {
+        query = query.filter(metadata_creators::creator_address.eq(any(creators)));
+    }
+    if let Some(owners) = owners {
+        query = query
             .filter(token_accounts::amount.eq(1))
-            .filter(token_accounts::owner_address.eq(any(owners)))
-            .select((
-                metadatas::address,
-                metadatas::name,
-                metadatas::seller_fee_basis_points,
-                metadatas::mint_address,
-                metadatas::primary_sale_happened,
-                metadata_jsons::description,
-                metadata_jsons::image,
-            ))
-            .order_by(metadatas::name.desc())
-            .load(conn)
-            .context("failed to load nft(s)")?
-    } else {
-        unreachable!("something has gone horribly wrong on NFTs query");
-    };
+            .filter(token_accounts::owner_address.eq(any(owners)));
+    }
+
+    let test_query = query
+        .select((
+            metadatas::address,
+            metadatas::name,
+            metadatas::seller_fee_basis_points,
+            metadatas::mint_address,
+            metadatas::primary_sale_happened,
+            metadata_jsons::description,
+            metadata_jsons::image,
+        ));
+
+        // let sql = debug_query::<diesel::pg::Pg, _>(&test_query);
+        // let result = sql.to_string().replace("\"", "");
+        // debug!("{:?}", result);
+
+    let rows: Vec<Nft> = test_query.load(conn)
+        .context("failed to load nft(s)")?;
 
     Ok(rows.into_iter().map(Into::into).collect())
 }
