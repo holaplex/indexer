6961af29cb2e65871a8b7c0254e4b57e6411d77c
Abdul Basit <baronbasit@gmail.com>
2022-03-15 00:10:59 +0500

    added required changes

--- crates/core/migrations/2022-03-11-192753_create_graph_connections_table/up.sql
+++ crates/core/migrations/2022-03-11-192753_create_graph_connections_table/up.sql
@@ -2,7 +2,7 @@ create table graph_connections (
   address           varchar(48) primary key,
   from_account      varchar(48) not null,
   to_account        varchar(48) not null
-  );
+);
 
 create index if not exists graph_connections_from_addr_idx
 on graph_connections (from_account);
