8c711098437d553ea138378b7eb03e29b653a1a3
raykast <rookie1286@gmail.com>
2022-03-22 11:53:44 -0700

    Got a preliminary version of the analytics query.

--- crates/core/src/db/queries/mint_stats.rs
+++ crates/core/src/db/queries/mint_stats.rs
@@ -1,11 +1,12 @@
 //! Retrieve per-mint statistics for an auction house.
 
 use anyhow::Context;
+use chrono::Local;
 use diesel::{
     pg::Pg,
     prelude::*,
     serialize::ToSql,
-    sql_types::{Array, Text},
+    sql_types::{Array, Text, Timestamp},
 };
 
 use crate::{
@@ -15,14 +16,33 @@ use crate::{
 
 const QUERY: &str = r"
 select
-    ah.address as auction_house,
-    ''::text as mint,
-    0::bigint as floor,
-    0::bigint as average,
-    0::bigint as volume_24hr,
-    0::bigint as count
+    ah.address                      as auction_house,
+    ah.treasury_mint                as mint,
+    min(pr.price)::bigint           as floor,
+    round(avg(pr.price))::bigint    as average,
+
+    sum(case
+        when ($2 - pr.created_at) < interval '24 hr'
+            then pr.price
+        else 0
+    end)::bigint as volume_24hr,
+
+    sum(c.count)::bigint as count
+
 from auction_houses ah
-where ah.address = any($1);
+    inner join purchase_receipts pr
+        on (pr.auction_house = ah.address)
+
+    inner join (
+        select lr.auction_house, count(distinct lr.metadata)
+        from listing_receipts lr
+        group by lr.auction_house
+    ) c on (c.auction_house = ah.address)
+
+where ah.address = any($1)
+group by ah.address;
+-- $1: auction house addresses::text[]
+-- $2: now::timestamp
 ";
 
 /// Load per-mint statistics for the given auction house address
@@ -35,6 +55,7 @@ pub fn load(
 ) -> Result<Vec<MintStats>> {
     diesel::sql_query(QUERY)
         .bind(auction_houses)
+        .bind::<Timestamp, _>(Local::now().naive_utc())
         .load(conn)
         .context("Failed to load mint stats")
 }
