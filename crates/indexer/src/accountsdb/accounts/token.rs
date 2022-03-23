5ff65a5c13cfc8ed2d127a898f2b14f2d3a7f357
Abdul Basit <baronbasit@gmail.com>
2022-03-23 05:59:13 +0500

    slot col for tk

--- crates/indexer/src/accountsdb/accounts/token.rs
+++ crates/indexer/src/accountsdb/accounts/token.rs
@@ -1,6 +1,6 @@
 use chrono::offset::Local;
 use indexer_core::{
-    db::{insert_into, models::TokenAccount as TokenAccountModel, tables::token_accounts},
+    db::{insert_into, models::TokenAccount as TokenAccountModel, tables::token_accounts, update},
     prelude::*,
 };
 use spl_token::state::Account as TokenAccount;
@@ -8,10 +8,14 @@ use spl_token::state::Account as TokenAccount;
 use super::Client;
 use crate::prelude::*;
 
-pub async fn process(client: &Client, key: Pubkey, token_account: TokenAccount) -> Result<()> {
+pub async fn process(
+    client: &Client,
+    key: Pubkey,
+    token_account: TokenAccount,
+    slot: u64,
+) -> Result<()> {
     let pubkey = key.to_string();
 
-    let now = Local::now().naive_utc();
     let amount: i64 = token_account
         .amount
         .try_into()
@@ -22,26 +26,59 @@ pub async fn process(client: &Client, key: Pubkey, token_account: TokenAccount) 
         return Ok(());
     }
 
+    let rows = client
+        .db()
+        .run(move |db| {
+            token_accounts::table
+                .select(token_accounts::all_columns)
+                .filter(token_accounts::address.eq(key.to_string()))
+                .load::<TokenAccountModel>(db)
+        })
+        .await
+        .context("failed to load")?;
+
     let values = TokenAccountModel {
-        address: Owned(pubkey),
+        address: Owned(pubkey.clone()),
         amount,
         mint_address: Owned(token_account.mint.to_string()),
         owner_address: Owned(owner),
-        updated_at: now,
+        updated_at: Local::now().naive_utc(),
+        slot: Some(slot.try_into()?),
     };
 
-    client
-        .db()
-        .run(move |db| {
-            insert_into(token_accounts::table)
-                .values(&values)
-                .on_conflict(token_accounts::address)
-                .do_update()
-                .set(&values)
-                .execute(db)
-        })
-        .await
-        .context("failed to insert token account")?;
+    if rows.len() == 1 {
+        let token_account = rows.get(0).context("err")?;
+
+        if let Some(s) = token_account.slot {
+            if i64::try_from(slot)? > s {
+                client
+                    .db()
+                    .run(move |db| {
+                        update(
+                            token_accounts::table
+                                .filter(token_accounts::address.eq(values.clone().address)),
+                        )
+                        .set(&values)
+                        .execute(db)
+                    })
+                    .await
+                    .context("failed to update token account")?;
+            } else {
+                return Ok(());
+            }
+        }
+    } else {
+        client
+            .db()
+            .run(move |db| {
+                insert_into(token_accounts::table)
+                    .values(&values)
+                    .on_conflict_do_nothing()
+                    .execute(db)
+            })
+            .await
+            .context("failed to insert token account")?;
+    }
 
     Ok(())
 }
