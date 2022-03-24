a3c31443d87691a414b28044ab3750f783214af7
Kyle Espinola <kyle.s.espinola@gmail.com>
2022-03-24 16:51:13 -0400

    Revert "Abdul/tk slot indexing (#267)"
    This reverts commit 808aa5b1b4d7f3e3dbb4be7d2294458776259a9d.

--- crates/core/migrations/2022-03-22-214148_add_slot_col_and_trigger_to_token_account/down.sql
+++
@@ -1,4 +0,0 @@
-alter table token_accounts
-drop column slot;
-drop trigger set_token_account_updated_at on token_accounts;
-drop function trigger_set_updated_at_timestamp();
--- crates/indexer/src/accountsdb/programs/token.rs
+++ crates/indexer/src/accountsdb/programs/token.rs
@@ -7,7 +7,7 @@ use crate::prelude::*;
 async fn process_token(client: &Client, update: AccountUpdate) -> Result<()> {
     let token_account = TokenAccount::unpack_unchecked(&update.data)
         .context("Failed to deserialize token account data!")?;
-    token::process(client, update.key, token_account, update.slot).await
+    token::process(client, update.key, token_account).await
 }
 
 pub(crate) async fn process(client: &Client, update: AccountUpdate) -> Result<()> {
