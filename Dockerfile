cardinal-labs/solana-indexer

WIP: Add instrumentation and Prometheus exporter.

Dockerfile
@@ -22,6 +22,7 @@ RUN cargo build --profile docker \
   --features " \
     metaplex-indexer/accountsdb, \
     metaplex-indexer/http \
+    metaplex-indexer-graphql/prometheus \
   " \
   --bin metaplex-indexer-accountsdb \
   --bin metaplex-indexer-http \

holaplex/indexer

Dockerfile
@@ -22,6 +22,7 @@ RUN cargo build --profile docker \
   --features " \
     holaplex-indexer/geyser, \
     holaplex-indexer/http \
+    holaplex-indexer-graphql/prometheus \
   " \
   --bin holaplex-indexer-geyser \
   --bin holaplex-indexer-http \