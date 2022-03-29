97f9fbd9b070737bd332a3b7a1cfad10938b9565
raykast <rookie1286@gmail.com>
2022-03-29 13:21:53 -0700

    Scaffolded geyser-config crate and update lockfile

---
+++ crates/geyser-config/src/main.rs
@@ -0,0 +1,14 @@
+//! GraphQL server for remotely managing `holaplex-indexer` Geyser configurations
+
+#![deny(
+    clippy::suspicious,
+    clippy::style,
+    missing_debug_implementations,
+    missing_copy_implementations
+)]
+#![warn(clippy::pedantic, clippy::cargo, missing_docs)]
+
+#[actix::main]
+async fn main() {
+    println!("Hello world!");
+}
