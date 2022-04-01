6d3ab40c275669b220878c227ad781096ff6b5b3
Abdul Basit <baronbasit@gmail.com>
2022-04-01 07:39:30 +0500

    added indexes

--- crates/core/migrations/2022-03-31-221314_create_table_metadata_collections_and_tokenstandard_col_for_metadatas/up.sql
+++ crates/core/migrations/2022-03-31-221314_create_table_metadata_collections_and_tokenstandard_col_for_metadatas/up.sql
@@ -5,6 +5,12 @@ create table metadata_collection_keys (
     primary key (metadata_address, collection_address)
 );
 
+create index if not exists metadata_collection_metadata_addr on 
+metadata_collection_keys using hash (metadata_address);
+
+create index if not exists metadata_collection_collection_addr on 
+metadata_collection_keys using hash (collection_address);
+
 create type token_standard 
 as enum ('NonFungible', 'FungibleAsset', 'Fungible', 'NonFungibleEdition');
 
