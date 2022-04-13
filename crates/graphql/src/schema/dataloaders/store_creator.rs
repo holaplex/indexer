7d6ab6b06806ed7f30d71b5cf1ab87826303576f
Anshul Goel <anshul.go87@gmail.com>
2022-04-13 14:08:02 +0530

    refactor: fetch twitter_handle, profile inside StoreCreator

--- crates/graphql/src/schema/dataloaders/store_creator.rs
+++ crates/graphql/src/schema/dataloaders/store_creator.rs
@@ -1,3 +1,4 @@
+use indexer_core::db::tables::twitter_handle_name_services;
 use objects::store_creator::StoreCreator;
 use scalars::{markers::StoreConfig, PublicKey};
 use tables::store_creators;
@@ -12,14 +13,21 @@ impl TryBatchFn<PublicKey<StoreConfig>, Vec<StoreCreator>> for Batcher {
     ) -> TryBatchMap<PublicKey<StoreConfig>, Vec<StoreCreator>> {
         let conn = self.db()?;
 
-        let rows: Vec<models::StoreCreator> = store_creators::table
+        let rows: Vec<(Option<String>, models::StoreCreator)> = store_creators::table
+            .left_join(twitter_handle_name_services::table.on(
+                twitter_handle_name_services::wallet_address.eq(store_creators::creator_address),
+            ))
             .filter(store_creators::store_config_address.eq(any(addresses)))
+            .select((
+                twitter_handle_name_services::twitter_handle.nullable(),
+                (store_creators::all_columns),
+            ))
             .load(&conn)
             .context("Failed to load store creator")?;
 
         Ok(rows
             .into_iter()
-            .map(|c| (c.store_config_address.clone(), c.try_into()))
+            .map(|c| (c.1.store_config_address.clone(), c.try_into()))
             .batch(addresses))
     }
 }
