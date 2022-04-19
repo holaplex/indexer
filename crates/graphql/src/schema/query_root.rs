4a3945d0670996d0b2500c425df3caead1b52c67
Jason Schorr <echohtp@gmail.com>
2022-04-18 20:34:57 -0400

    add context(...)

--- crates/graphql/src/schema/query_root.rs
+++ crates/graphql/src/schema/query_root.rs
@@ -94,7 +94,7 @@ impl QueryRoot {
             .filter(bid_receipts::canceled_at.is_null())
             .filter(bid_receipts::purchase_receipt.is_null())
             .filter(bid_receipts::metadata.eq(address))
-            .load(&conn)?;
+            .load(&conn).context("Failed to load bid_receipts")?;
 
         rows.into_iter()
             .map(TryInto::try_into)
