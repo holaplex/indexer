8ab2f38744b86185edc1ce6a8b01b9170b5cda11
Daniel <danieladammiller@gmail.com>
2022-04-25 07:58:04 -0700

    formatting for migrations

--- crates/core/migrations/2022-04-22-182428_add_source_column_to_twitter_handles/down.sql
+++ crates/core/migrations/2022-04-22-182428_add_source_column_to_twitter_handles/down.sql
@@ -1,9 +1,5 @@
 
 alter table twitter_handle_name_services
-drop column from_bonfida;
-
-alter table twitter_handle_name_services
-drop column from_cardinal;
-
-alter table twitter_handle_name_services
+drop column from_bonfida,
+drop column from_cardinal,
 drop column write_version;
--- crates/core/migrations/2022-04-22-182428_add_source_column_to_twitter_handles/up.sql
+++ crates/core/migrations/2022-04-22-182428_add_source_column_to_twitter_handles/up.sql
@@ -1,9 +1,5 @@
 
 alter table twitter_handle_name_services
-add column from_bonfida boolean not null default true;
-
-alter table twitter_handle_name_services
-add column from_cardinal boolean not null default false;
-
-alter table twitter_handle_name_services
+add column from_bonfida boolean not null default true,
+add column from_cardinal boolean not null default false,
 add column write_version bigint not null default 0;
