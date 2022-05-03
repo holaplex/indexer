7417db13d853d22ba4b4f64a945f47d79900594c
Kevin Rodriguez <_@kevinrodriguez.io>
2022-05-03 04:01:27 -0600

    chore: fixing sql

--- crates/core/migrations/2022-04-27-183240_create_graph_program_columns/up.sql
+++ crates/core/migrations/2022-04-27-183240_create_graph_program_columns/up.sql
@@ -3,7 +3,7 @@ add column connected_at timestamp,
 add column disconnected_at timestamp;
 
 update graph_connections
-set connected_at = CURRENT_TIMESTAMP();
+set connected_at = CURRENT_TIMESTAMP;
 
 alter table graph_connections
 alter column connected_at set not null;
