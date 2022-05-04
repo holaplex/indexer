136bd514bca60a3761cd0c59e0ac664994e11f8b
Kevin Rodriguez <_@kevinrodriguez.io>
2022-05-04 01:20:38 -0600

    feat: moving index to the right col

--- crates/core/migrations/2022-04-27-183240_create_graph_program_columns/up.sql
+++ crates/core/migrations/2022-04-27-183240_create_graph_program_columns/up.sql
@@ -2,7 +2,7 @@ alter table graph_connections
 add column connected_at timestamp,
 add column disconnected_at timestamp;
 
-create index on graph_connections (connected_at);
+create index on graph_connections (disconnected_at);
 
 update graph_connections
 set connected_at = CURRENT_TIMESTAMP;
