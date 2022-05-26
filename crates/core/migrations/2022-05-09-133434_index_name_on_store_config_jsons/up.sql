57dd0b086116948992d1c26e96231c1179a2edba
Austin Milt <austin@holaplex.com>
2022-05-26 11:18:37 -0600

    add constraint that subdomain is unique across rows

--- crates/core/migrations/2022-05-09-133434_index_name_on_store_config_jsons/up.sql
+++ crates/core/migrations/2022-05-09-133434_index_name_on_store_config_jsons/up.sql
@@ -1,2 +1,5 @@
+alter table store_config_jsons
+add constraint uniq_subdomain unique (subdomain);
+
 create index if not exists name_store_config_jsons_idx on
   store_config_jsons using btree (name);
