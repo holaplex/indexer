60524bc6ef754a9c47a2650a619e29fdfd1ec62a
Austin Milt <austin@holaplex.com>
2022-05-31 17:30:10 -0600

    fix migration

--- crates/core/migrations/2022-05-31-133434_index_name_on_store_config_jsons/up.sql
+++ crates/core/migrations/2022-05-31-133434_index_name_on_store_config_jsons/up.sql
@@ -1,3 +1,6 @@
+delete from store_config_jsons 
+  where store_address='3doAaFs2VuTLnVTPLZwFAWsskqwwC4xLt31dZ24uwYsd';
+
 alter table store_config_jsons
 add constraint uniq_subdomain unique (subdomain);
 
