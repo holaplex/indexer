751eec622a066a7cfe0ae52b9ba713abe79a3fe0
Anshul Goel <anshul.go87@gmail.com>
2022-06-08 13:31:44 +0530

    Fix: migration for store_auction_houses

--- crates/core/migrations/2022-06-08-060820_create_store_auction_houses_from_auction_house/up.sql
+++ crates/core/migrations/2022-06-08-060820_create_store_auction_houses_from_auction_house/up.sql
@@ -1,2 +1,2 @@
 insert into store_auction_houses (store_config_address, auction_house_address)
-  select (config_address as store_config_address, auction_house_address) from store_config_jsons;
+  select config_address, auction_house_address from store_config_jsons;
