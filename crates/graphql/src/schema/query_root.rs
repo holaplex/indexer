89a0f9bdc490df95f78c9f36e3599e763711224b
Austin Milt <austin@holaplex.com>
2022-06-09 15:28:56 -0600

    fix query to return wallet for all requested addresses

--- crates/graphql/src/schema/query_root.rs
+++ crates/graphql/src/schema/query_root.rs
@@ -413,12 +413,21 @@ impl QueryRoot {
         let conn = context.shared.db.get()?;
 
         let twitter_handles =
-            queries::twitter_handle_name_service::get_multiple(&conn, &addresses).unwrap();
+            queries::twitter_handle_name_service::get_multiple(&conn, &addresses)?;
 
-        Ok(twitter_handles
-            .into_iter()
-            .map(|h| Wallet::new(h.wallet_address.into(), Some(h.twitter_handle.into_owned())))
-            .collect())
+        let mut wallets: HashMap<String, Wallet> = HashMap::new();
+        twitter_handles.into_iter().for_each(|twitter_handle| {
+            wallets.insert(twitter_handle.wallet_address.to_string(), Wallet::new(twitter_handle.wallet_address.into(), Some(twitter_handle.twitter_handle.into())));
+        });
+
+        for address in addresses {
+            let address_string = address.to_string();
+            if !wallets.contains_key(&address_string) {
+                wallets.insert(address_string, Wallet::new(address, None));
+            }
+        }
+
+        Ok(wallets.values().cloned().collect())
     }
 
     fn listings(&self, context: &AppContext) -> FieldResult<Vec<Listing>> {
