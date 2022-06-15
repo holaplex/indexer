541d4f3a890fb60e79b09e53efb6a769a3612037
Austin Milt <austin@holaplex.com>
2022-06-15 16:07:33 -0600

    cleanup

--- crates/graphql/src/schema/query_root.rs
+++ crates/graphql/src/schema/query_root.rs
@@ -27,7 +27,7 @@ use tables::{
     metadata_jsons, metadatas, store_config_jsons, storefronts, wallet_totals,
 };
 
-use super::{prelude::*};
+use super::prelude::*;
 pub struct QueryRoot;
 
 #[derive(GraphQLInputObject, Clone, Debug)]
