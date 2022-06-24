94a179fcc849a62ecd38b6b7971c449f2bc42ff6
Abdul Basit <baronbasit@gmail.com>
2022-06-24 23:31:41 +0500

    expose all the fields of listing and offer

--- crates/graphql/src/schema/objects/ah_listing.rs
+++ crates/graphql/src/schema/objects/ah_listing.rs
@@ -8,12 +8,15 @@ use super::prelude::*;
 pub struct AhListing {
     pub id: Uuid,
     pub trade_state: String,
-    pub seller: PublicKey<Wallet>,
     pub auction_house: PublicKey<AuctionHouse>,
+    pub seller: PublicKey<Wallet>,
     pub metadata: PublicKey<Nft>,
+    pub purchase_id: Option<Uuid>,
     pub price: U64,
     pub token_size: i32,
+    pub trade_state_bump: i32,
     pub created_at: DateTime<Utc>,
+    pub canceled_at: Option<DateTime<Utc>>,
 }
 
 #[graphql_object(Context = AppContext)]
@@ -27,28 +30,40 @@ impl AhListing {
         &self.trade_state
     }
 
-    fn seller(&self) -> &PublicKey<Wallet> {
-        &self.seller
-    }
-
     fn auction_house(&self) -> &PublicKey<AuctionHouse> {
         &self.auction_house
+    }
+
+    fn seller(&self) -> &PublicKey<Wallet> {
+        &self.seller
     }
 
     fn metadata(&self) -> &PublicKey<Nft> {
         &self.metadata
     }
 
+    fn purchase_id(&self) -> Option<Uuid> {
+        self.purchase_id
+    }
+
     fn price(&self) -> U64 {
         self.price
+    }
+
+    fn token_size(&self) -> i32 {
+        self.token_size
+    }
+
+    fn trade_state_bump(&self) -> i32 {
+        self.trade_state_bump
     }
 
     fn created_at(&self) -> DateTime<Utc> {
         self.created_at
     }
 
-    fn token_size(&self) -> i32 {
-        self.token_size
+    fn canceled_at(&self) -> Option<DateTime<Utc>> {
+        self.canceled_at
     }
 
     pub async fn nft(&self, ctx: &AppContext) -> FieldResult<Option<Nft>> {
@@ -68,9 +83,12 @@ impl<'a> TryFrom<models::Listing<'a>> for AhListing {
             auction_house,
             seller,
             metadata,
+            purchase_id,
             price,
             token_size,
+            trade_state_bump,
             created_at,
+            canceled_at,
             ..
         }: models::Listing,
     ) -> Result<Self, Self::Error> {
@@ -80,9 +98,12 @@ impl<'a> TryFrom<models::Listing<'a>> for AhListing {
             auction_house: auction_house.into_owned().into(),
             seller: seller.into_owned().into(),
             metadata: metadata.into_owned().into(),
+            purchase_id,
             price: price.try_into()?,
-            created_at: DateTime::from_utc(created_at, Utc),
             token_size: token_size.try_into()?,
+            trade_state_bump: trade_state_bump.into(),
+            created_at: DateTime::from_utc(created_at, Utc),
+            canceled_at: canceled_at.map(|c| DateTime::from_utc(c, Utc)),
         })
     }
 }
