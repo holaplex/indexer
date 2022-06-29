8fec93cdba49569692a5f6493808bf369a94fd15
Austin Milt <austin@holaplex.com>
2022-06-29 09:53:27 -0600

    formatting

--- crates/core/src/db/queries/collections.rs
+++ crates/core/src/db/queries/collections.rs
@@ -110,7 +110,7 @@ fn make_by_market_cap_query_string(order_direction: OrderDirection) -> String {
             FROM listing_receipts
             INNER JOIN metadatas on (listing_receipts.metadata = metadatas.address)
             INNER JOIN metadata_collection_keys on (metadatas.address = metadata_collection_keys.metadata_address)
-            WHERE 
+            WHERE
                 ($1 IS NULL OR metadata_collection_keys.collection_address = ANY($1))
                 AND listing_receipts.purchase_receipt IS NULL
                 AND listing_receipts.canceled_at IS NULL
