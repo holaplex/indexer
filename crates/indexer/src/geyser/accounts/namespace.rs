464df92289a72a4e4455332e56d91df64e2db86e
Abdul Basit <baronbasit@gmail.com>
2022-07-14 03:00:04 +0500

    Indexing cardinal namespace and entries accounts

--- crates/indexer/src/geyser/accounts/namespace.rs
+++ crates/indexer/src/geyser/accounts/namespace.rs
@@ -1,8 +1,13 @@
 use indexer_core::{
-    db::{insert_into, models::TwitterHandle, tables::twitter_handle_name_services, update},
+    db::{
+        insert_into,
+        models::{CardinalEntry, CardinalNamespace, TwitterHandle},
+        tables::{cardinal_entries, cardinal_namespaces, twitter_handle_name_services},
+        update,
+    },
     pubkeys::CARDINAL_TWITTER_NAMESPACE,
 };
-use namespaces::state::Entry;
+use namespaces::state::{Entry, Namespace};
 
 use super::Client;
 use crate::{prelude::*, search_dispatch::TwitterHandleDocument};
@@ -14,10 +19,86 @@ pub(crate) async fn process(
     write_version: u64,
     entry: Entry,
 ) -> Result<()> {
-    if entry.namespace != CARDINAL_TWITTER_NAMESPACE {
-        return Ok(());
+    if entry.namespace == CARDINAL_TWITTER_NAMESPACE {
+        process_twitter_namespace(client, key, slot, write_version, entry.clone())
+            .await
+            .context("failed to process twitter namespace")?;
     }
 
+    let row = CardinalEntry {
+        address: Owned(key.to_string()),
+        namespace: Owned(entry.namespace.to_string()),
+        name: Owned(entry.clone().name),
+        data: entry.data.map(|a| Owned(a.to_string())),
+        reverse_entry: entry.reverse_entry.map(|a| Owned(a.to_string())),
+        mint: Owned(entry.mint.to_string()),
+        is_claimed: entry.is_claimed,
+        slot: slot.try_into()?,
+        write_version: write_version.try_into()?,
+    };
+
+    client
+        .db()
+        .run(move |db| {
+            insert_into(cardinal_entries::table)
+                .values(&row)
+                .on_conflict(cardinal_entries::address)
+                .do_update()
+                .set(&row)
+                .execute(db)
+        })
+        .await
+        .context("failed to insert cardinal entry")?;
+
+    Ok(())
+}
+
+pub(crate) async fn process_namespace(
+    client: &Client,
+    key: Pubkey,
+    slot: u64,
+    write_version: u64,
+    namespace: Namespace,
+) -> Result<()> {
+    let row = CardinalNamespace {
+        address: Owned(key.to_string()),
+        name: Owned(namespace.name.to_string()),
+        update_authority: Owned(namespace.clone().update_authority.to_string()),
+        rent_authority: Owned(namespace.rent_authority.to_string()),
+        approve_authority: namespace.approve_authority.map(|a| Owned(a.to_string())),
+        schema: namespace.schema.try_into()?,
+        payment_amount_daily: namespace.payment_amount_daily.try_into()?,
+        payment_mint: Owned(namespace.payment_mint.to_string()),
+        min_rental_seconds: namespace.min_rental_seconds,
+        max_rental_seconds: namespace.max_rental_seconds,
+        transferable_entries: namespace.transferable_entries,
+        slot: slot.try_into()?,
+        write_version: write_version.try_into()?,
+    };
+
+    client
+        .db()
+        .run(move |db| {
+            insert_into(cardinal_namespaces::table)
+                .values(&row)
+                .on_conflict(cardinal_namespaces::address)
+                .do_update()
+                .set(&row)
+                .execute(db)
+        })
+        .await
+        .context("failed to insert cardinal namespace")?;
+
+    Ok(())
+}
+
+async fn process_twitter_namespace(
+    client: &Client,
+    key: Pubkey,
+    slot: u64,
+    write_version: u64,
+    entry: Entry,
+) -> Result<()> {
     let slot = i64::try_from(slot)?;
     let write_version = i64::try_from(write_version)?;
 
--- crates/indexer/src/geyser/programs/namespaces.rs
+++ crates/indexer/src/geyser/programs/namespaces.rs
@@ -1,16 +1,30 @@
 use anchor_lang_v0_20::AccountDeserialize;
 use indexer_core::prelude::*;
-use namespaces::state::{Entry, ENTRY_SIZE};
+use namespaces::state::{Entry, Namespace, ENTRY_SIZE, NAMESPACE_SIZE};
 
 use super::{accounts::namespace, AccountUpdate, Client};
 
 pub(crate) async fn process(client: &Client, update: AccountUpdate) -> Result<()> {
-    if update.data.len() == ENTRY_SIZE {
-        let entry: Entry = Entry::try_deserialize(&mut update.data.as_slice())
-            .context("Failed to deserialize cardinal entry")?;
+    match update.data.len() {
+        ENTRY_SIZE => {
+            let entry: Entry = Entry::try_deserialize(&mut update.data.as_slice())
+                .context("Failed to deserialize cardinal entry")?;
 
-        namespace::process(client, update.key, update.slot, update.write_version, entry).await?;
+            namespace::process(client, update.key, update.slot, update.write_version, entry).await
+        },
+        NAMESPACE_SIZE => {
+            let namespace: Namespace = Namespace::try_deserialize(&mut update.data.as_slice())
+                .context("Failed to deserialize namespace")?;
+
+            namespace::process_namespace(
+                client,
+                update.key,
+                update.slot,
+                update.write_version,
+                namespace,
+            )
+            .await
+        },
+        _ => Ok(()),
     }
-
-    Ok(())
 }
