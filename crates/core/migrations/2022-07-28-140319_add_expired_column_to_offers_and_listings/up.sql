9f2ab13f35da3c49fe8cc6051c0ab827bf0f0147
Abdul Basit <baronbasit@gmail.com>
2022-07-28 22:23:07 +0500

    add indexes

--- crates/core/migrations/2022-07-28-140319_add_expired_column_to_offers_and_listings/up.sql
+++ crates/core/migrations/2022-07-28-140319_add_expired_column_to_offers_and_listings/up.sql
@@ -3,3 +3,6 @@ add column expiry timestamp;
 
 alter table listings
 add column expiry timestamp;
+
+create index if not exists listings_expiry_idx on listings(expiry);
+create index if not exists offers_expiry_idx on offers(expiry);
