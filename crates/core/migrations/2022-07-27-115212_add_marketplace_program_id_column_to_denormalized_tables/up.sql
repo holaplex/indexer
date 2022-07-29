6f3da01f0623fb94c7b3816052a008a2ef429b80
Abdul Basit <baronbasit@gmail.com>
2022-07-29 18:58:34 +0500

    edit marketplace_program migration

--- crates/core/migrations/2022-07-27-115212_add_marketplace_program_id_column_to_denormalized_tables/up.sql
+++ crates/core/migrations/2022-07-27-115212_add_marketplace_program_id_column_to_denormalized_tables/up.sql
@@ -1,38 +1,8 @@
 alter table offers
-add marketplace_program varchar(48);
+add marketplace_program varchar(48) not null default 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K';
 
 alter table listings
-add marketplace_program varchar(48);
+add marketplace_program varchar(48) not null default 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K';
 
 alter table purchases
-add marketplace_program varchar(48);
-
--- backfill marketplace_program column
-update offers
-set marketplace_program = 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K'
-where offers.auction_house = 'E8cU1WiRWjanGxmn96ewBgk9vPTcL6AEZ1t6F6fkgUWe';
-
-update listings
-set marketplace_program = 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K'
-where listings.auction_house = 'E8cU1WiRWjanGxmn96ewBgk9vPTcL6AEZ1t6F6fkgUWe';
-
-update purchases
-set marketplace_program = 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K'
-where purchases.auction_house = 'E8cU1WiRWjanGxmn96ewBgk9vPTcL6AEZ1t6F6fkgUWe';
-
-update offers
-set marketplace_program = 'hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk'
-where offers.auction_house != 'E8cU1WiRWjanGxmn96ewBgk9vPTcL6AEZ1t6F6fkgUWe';
-
-update listings
-set marketplace_program = 'hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk'
-where listings.auction_house != 'E8cU1WiRWjanGxmn96ewBgk9vPTcL6AEZ1t6F6fkgUWe';
-
-update purchases
-set marketplace_program = 'hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk'
-where purchases.auction_house != 'E8cU1WiRWjanGxmn96ewBgk9vPTcL6AEZ1t6F6fkgUWe';
-
---add not null constraint
-alter table offers alter column marketplace_program set not null;
-alter table listings alter column marketplace_program set not null;
-alter table purchases alter column marketplace_program set not null;
+add marketplace_program varchar(48) not null default 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K';
