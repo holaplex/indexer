56326f51b3f8f6905ee96af3c6247a1578d5917f
Abdul Basit <baronbasit@gmail.com>
2022-07-29 21:40:50 +0500

    add if not exists condition to migration

--- crates/core/migrations/2022-07-28-140319_add_expired_column_to_offers_and_listings/up.sql
+++ crates/core/migrations/2022-07-28-140319_add_expired_column_to_offers_and_listings/up.sql
@@ -1,8 +1,8 @@
 alter table offers
-add column expiry timestamp;
+add column if not exists expiry timestamp;
 
 alter table listings
-add column expiry timestamp;
+add column if not exists expiry timestamp;
 
 create index if not exists listings_expiry_idx on listings(expiry);
 create index if not exists offers_expiry_idx on offers(expiry);
