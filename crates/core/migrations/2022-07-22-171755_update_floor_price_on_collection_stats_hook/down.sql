67491bcf56815e06ff8297dd05a038652f9279f9
Austin Milt <austin@holaplex.com>
2022-08-05 11:43:34 -0600

    fix down migration

--- crates/core/migrations/2022-07-22-171755_update_floor_price_on_collection_stats_hook/down.sql
+++ crates/core/migrations/2022-07-22-171755_update_floor_price_on_collection_stats_hook/down.sql
@@ -1,1 +1,3 @@
-SELECT FALSE;
+drop trigger listing_added on listings;
+drop trigger listing_updated on listings;
+drop function floor_price_on_listing_update();
--- crates/core/migrations/2022-08-05-172611_fix_update_floor_price_on_collection_stats_hook/down.sql
+++ crates/core/migrations/2022-08-05-172611_fix_update_floor_price_on_collection_stats_hook/down.sql
@@ -1,3 +1,1 @@
-drop trigger listing_added on listings;
-drop trigger listing_updated on listings;
-drop function floor_price_on_listing_update();
+SELECT FALSE;
