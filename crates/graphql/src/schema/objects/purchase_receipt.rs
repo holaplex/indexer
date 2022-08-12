66dc8e8fd3d7fe6c39bd37c59ea2fe733cb2ce54
Anshul Goel <anshul.go87@gmail.com>
2022-08-13 03:58:22 +0530

    Same dataloaders with different names (#616)
    * fix: same dataloaders with different names
    
    * cleanup
    
    * fix: call the correct loader for walletactivity auction_house

--- crates/graphql/src/schema/objects/purchase_receipt.rs
+++ crates/graphql/src/schema/objects/purchase_receipt.rs
@@ -50,7 +50,7 @@ impl PurchaseReceipt {
 
     pub async fn auction_house(&self, context: &AppContext) -> FieldResult<Option<AuctionHouse>> {
         context
-            .store_auction_houses_loader
+            .auction_house_loader
             .load(self.auction_house.clone())
             .await
             .map_err(Into::into)
