b4c464f78e7d18b689d52887d73268f1efcac66a
William Barksdale <wfbarksdale@gmail.com>
2022-08-17 12:36:30 -0500

    use compound key for candy machine creators (#673)
    * use compound key for candy machine creators
    
    * drop duplicates when migrating down
    
    * update schema

---
+++ crates/core/migrations/2022-08-15-172917_add_compound_key_to_candymachine_creators/up.sql
@@ -0,0 +1,3 @@
+alter table candy_machine_creators
+drop constraint candy_machine_creators_pkey,
+add constraint candy_machine_creators_compound_pkey primary key (candy_machine_address, creator_address);
