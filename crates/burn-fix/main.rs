9ea71eb9892503a9924e1ee96c2faba597473646
ryans <me@ryan-s.net>
2022-08-17 11:00:09 -0700

    Refactored burn-fix.

--- crates/burn-fix/main.rs
+++ crates/burn-fix/main.rs
@@ -1,34 +1,39 @@
+#![allow(clippy::pedantic, clippy::cargo)]
+
+use indexer::prelude::*;
 use indexer_core::{
     clap,
     clap::Parser,
     db,
-    db::{tables::metadatas, update, Connection},
-    prelude::*,
+    db::{tables::metadatas, update},
 };
 use solana_client::{
     client_error::{ClientError, ClientErrorKind},
     rpc_client::RpcClient,
     rpc_request::RpcError,
 };
-use solana_program::pubkey::Pubkey;
 
 #[derive(Debug, Parser)]
 struct Opts {
+    /// Solana RPC endpoint
+    #[clap(long, env)]
+    solana_endpoint: String,
+
     #[clap(flatten)]
     db: db::ConnectArgs,
-    #[clap(env)]
-    solana_endpoint: String,
 }
 
 fn main() {
     indexer_core::run(|| {
+        let opts = Opts::parse();
+        debug!("{:#?}", opts);
+
         let Opts {
-            db,
             solana_endpoint,
-        } = Opts::parse();
+            db,
+        } = opts;
 
         let client = RpcClient::new(&solana_endpoint);
-        debug!("USING SOLANA ENDPOINT: {:?}", &solana_endpoint);
 
         let db::ConnectResult {
             pool,
@@ -37,57 +42,61 @@ fn main() {
         } = db::connect(db, db::ConnectMode::Write { migrate: false })?;
 
         let conn = pool.get()?;
-        let eariliest_burn_slot = metadatas::table
+        let earliest_burn_slot = metadatas::table
             .filter(metadatas::burned_at.is_not_null())
             .order_by(metadatas::slot.asc())
             .select(metadatas::slot)
             .first::<Option<i64>>(&conn)
             .ok()
             .flatten()
-            .context("could not load earliest slot")?;
+            .context("Could not load earliest slot")?;
 
         // TODO: Check for index on burned_at
         let total_count = metadatas::table
             .filter(metadatas::burned_at.is_null())
-            .filter(metadatas::slot.lt(eariliest_burn_slot))
+            .filter(metadatas::slot.lt(earliest_burn_slot))
             .count()
             .get_result::<i64>(&conn)
-            .context("could not get count")?;
+            .context("Could not get count")?;
 
-        debug!("TOTAL UNBURNED COUNT: {:?}", total_count);
+        debug!("Total unburned count: {}", total_count);
 
-        let mut i = 0;
-        let limit = 1000;
-        while i < total_count {
+        const LIMIT: i64 = 1000;
+
+        for i in (0..total_count).step_by(LIMIT as usize) {
             // slot less than the earliest burn slot;
             let addressess = metadatas::table
                 .filter(metadatas::burned_at.is_null())
-                .filter(metadatas::slot.lt(eariliest_burn_slot))
+                .filter(metadatas::slot.lt(earliest_burn_slot))
                 .offset(i)
-                .limit(limit)
+                .limit(LIMIT)
                 .order_by(metadatas::slot.asc())
                 .select(metadatas::address)
                 .load(&conn)
-                .context("could not load metadatas")?;
+                .context("Could not load metadatas")?;
 
             addressess.into_iter().try_for_each(|address: String| {
                 let key: Pubkey = address.parse()?;
                 let exists = account_exists(key, &client);
+
                 if let Ok(false) = exists {
-                    debug!("manually burning: {:?}", address);
+                    debug!("Manually burning: {:?}", address);
                     remove_metadata(address, &conn, &client)?;
                 }
+
                 Result::<_>::Ok(())
             })?;
-
-            i += limit;
         }
 
         Ok(())
     });
 }
 
-fn remove_metadata(metadata_address: String, conn: &Connection, client: &RpcClient) -> Result<()> {
+fn remove_metadata(
+    metadata_address: String,
+    conn: &db::Connection,
+    client: &RpcClient,
+) -> Result<()> {
     let now = Local::now().naive_utc();
     let slot = i64::try_from(client.get_slot().context("failed to get slot")?)?;
 
