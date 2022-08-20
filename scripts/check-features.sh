3e9c0688b2547178af3d8a6eb6105b36598b1e09
Ryan S <me@ryan-s.net>
2022-08-19 18:28:44 -0700

    Fixed logic errors in check-features script. (#686)

--- scripts/check-features.sh
+++ scripts/check-features.sh
@@ -4,10 +4,28 @@ set -e
 shopt -s globstar
 cd "$(dirname "$0")/.."
 
+function puts() {
+  if [[ -t 1 ]]; then
+    echo "$@"
+  else
+    sed 's/\x1b\[[^m]*m//g' <<<"$@"
+  fi
+}
+
+function putsc() {
+  puts $'\x1b'"[$1m""$2"$'\x1b[m'
+}
+
 function cc() {
   m="$1"
   shift 1
-  cargo "${cmdbase[@]}" --manifest-path "$m" --no-default-features "$@"
+  cmd=(cargo "${cmdbase[@]}" --manifest-path "$m" --no-default-features "$@")
+
+  if [[ -n "$dry_run" ]]; then
+    puts "${cmd[@]}"
+  else
+    "${cmd[@]}"
+  fi
 }
 
 function ccr() {
@@ -15,50 +33,73 @@ function ccr() {
 }
 
 cmdbase=(check)
+dry_run=''
 release=''
 passes=(1)
 
-while getopts cr2 opt; do
+while getopts cnr2 opt; do
   case "$opt" in
     c) cmdbase=(clippy --no-deps) ;;
+    n) dry_run=1 ;;
     r) release=1 ;;
     2) passes=({1..2}) ;;
   esac
 done
 
 for i in "${passes[@]}"; do
-  echo $'\x1b[1m'":: Pass $i"$'\x1b[m'
+  putsc 1 ":: Pass $i"
 
   for f in **/Cargo.toml; do
     json="$(cargo read-manifest --manifest-path "$f" 2>/dev/null)" || continue
 
-    name="$(jq -r .name <<<"$json")" || continue
-
-    [[ -n "$name" ]] || continue
-    echo $'\x1b[1m'" => $name"$'\x1b[m'
-
-    echo "  -> No features"
-    cc "$f"
-
-    if [[ -n "$release" ]]; then
-      echo "  -> No features (release)"
-      ccr "$f"
-    fi
-
     features="$(jq -r '.features | keys | join("\n")' <<<"$json")"
+    targets="$(jq -r '.targets | keys | join("\n")' <<<"$json")"
 
-    for feat in $features; do
-      [[ -n "$feat" ]] || continue
+    for tgt in $targets; do
+      tj="$(jq -r ".targets[$tgt]" <<<"$json")"
 
-      args=("$f" --features "$feat")
+      name="$(jq -r '.name' <<<"$tj")"
+      kinds="$(jq -r '.kind | join("\n")' <<<"$tj")"
+      req_features="$(jq -r '.["required-features"] | join(",")?' <<<"$tj")"
 
-      echo "  -> $feat"
-      cc "${args[@]}"
-
-      if [[ -n "$release" ]]; then
-        echo "  -> $feat (release)"
-        ccr "${args[@]}"
+      if [[ -n "$req_features" ]]; then
+        tgt_flags=(--features "$req_features")
+      else
+        tgt_flags=()
       fi
+
+      for kind in $kinds; do
+        putsc 1 " => $name ($kind)"
+
+        case "$kind" in
+          lib) kind_flags=(--lib) ;;
+          *) kind_flags=("--$kind" "$name") ;;
+        esac
+
+        cc_flags=("$f" "${tgt_flags[@]}" "${kind_flags[@]}")
+
+        putsc '38;5;4' '  -> No features'
+        cc "${cc_flags[@]}"
+
+        if [[ -n "$release" ]]; then
+          putsc '38;5;5' '  -> No features (release)'
+          ccr "${cc_flags[@]}"
+        fi
+
+        for feat in $features; do
+          [[ -n "$feat" ]] || continue
+
+          cc_feat_flags=("${cc_flags[@]}" --features "$feat")
+
+          putsc '38;5;4' "  -> $feat"
+          cc "${cc_feat_flags[@]}"
+
+          if [[ -n "$release" ]]; then
+            putsc '38;5;5' "  -> $feat (release)"
+            ccr "${cc_feat_flags[@]}"
+          fi
+        done
+      done
     done
   done
 done
