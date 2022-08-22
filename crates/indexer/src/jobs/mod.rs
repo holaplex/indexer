d639c5d3cba6dbdf869fff4812e59969abebcafb
ryans <me@ryan-s.net>
2022-08-22 16:41:12 -0700

    Got the job runner and dispatcher communicating.

--- crates/indexer/src/jobs/mod.rs
+++ crates/indexer/src/jobs/mod.rs
@@ -7,12 +7,17 @@ use indexer_rabbitmq::job_runner::Message;
 use crate::prelude::*;
 
 /// Message identifier
-#[derive(Debug, Clone, Copy)]
-pub enum MessageId {}
+#[derive(Debug, Clone)]
+pub enum MessageId {
+    /// A refresh of a cache table
+    RefreshTable(String),
+}
 
 impl fmt::Display for MessageId {
-    fn fmt(&self, _: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {}
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::RefreshTable(n) => write!(f, "refresh of cached table {}", n),
+        }
     }
 }
 
@@ -21,6 +26,18 @@ impl fmt::Display for MessageId {
 /// # Errors
 /// This function fails if an error occurs processing the message body.
 pub async fn process_message(msg: Message) -> MessageResult<MessageId> {
-    match msg {}
-    std::future::pending().await
+    let id = match msg {
+        Message::RefreshTable(ref n) => MessageId::RefreshTable(n.clone()),
+    };
+
+    match msg {
+        Message::RefreshTable(n) => process_refresh(n).await,
+    }
+    .map_err(|e| MessageError::new(e, id))
+}
+
+async fn process_refresh(name: String) -> Result<()> {
+    debug!("Refreshing table {:?}", name);
+
+    Ok(())
 }
