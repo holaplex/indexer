e36b5cd516f92fcb63be487a8dd612598cb46e76
Abdul Basit <baronbasit@gmail.com>
2022-09-06 09:34:51 +0500

    Fix Burn job to handle partial burned NFTs

--- crates/burn-fix/main.rs
+++ crates/burn-fix/main.rs
@@ -5,7 +5,10 @@ use indexer_core::{
     clap,
     clap::Parser,
     db,
-    db::{tables::metadatas, update},
+    db::{
+        tables::{current_metadata_owners, metadatas},
+        update,
+    },
 };
 use solana_client::{
     client_error::{ClientError, ClientErrorKind},
@@ -42,74 +45,70 @@ fn main() {
         } = db::connect(db, db::ConnectMode::Write { migrate: false })?;
 
         let conn = pool.get()?;
-        let earliest_burn_slot = metadatas::table
-            .filter(metadatas::burned_at.is_not_null())
-            .order_by(metadatas::slot.asc())
-            .select(metadatas::slot)
-            .first::<Option<i64>>(&conn)
-            .ok()
-            .flatten()
-            .context("Could not load earliest slot")?;
 
-        // TODO: Check for index on burned_at
         let total_count = metadatas::table
+            .inner_join(
+                current_metadata_owners::table
+                    .on(current_metadata_owners::mint_address.eq(metadatas::mint_address)),
+            )
             .filter(metadatas::burned_at.is_null())
-            .filter(metadatas::slot.lt(earliest_burn_slot))
             .count()
             .get_result::<i64>(&conn)
             .context("Could not get count")?;
 
         debug!("Total unburned count: {}", total_count);
 
-        const LIMIT: i64 = 1000;
+        const LIMIT: i64 = 10000;
 
         for i in (0..total_count).step_by(LIMIT as usize) {
-            // slot less than the earliest burn slot;
-            let addressess = metadatas::table
+            let addresses = metadatas::table
+                .inner_join(
+                    current_metadata_owners::table
+                        .on(current_metadata_owners::mint_address.eq(metadatas::mint_address)),
+                )
                 .filter(metadatas::burned_at.is_null())
-                .filter(metadatas::slot.lt(earliest_burn_slot))
                 .offset(i)
                 .limit(LIMIT)
                 .order_by(metadatas::slot.asc())
-                .select(metadatas::address)
+                .select((
+                    current_metadata_owners::token_account_address,
+                    metadatas::address,
+                ))
                 .load(&conn)
                 .context("Could not load metadatas")?;
 
-            addressess.into_iter().try_for_each(|address: String| {
-                let key: Pubkey = address.parse()?;
-                let exists = account_exists(key, &client);
+            addresses
+                .into_iter()
+                .try_for_each(|(token_address, metadata): (String, String)| {
+                    let key: Pubkey = token_address.parse()?;
+                    let exists = account_exists(key, &client);
 
-                if let Ok(false) = exists {
-                    debug!("Manually burning: {:?}", address);
-                    remove_metadata(address, &conn, &client)?;
-                }
+                    if let Ok(false) = exists {
+                        debug!("Manually burning: {:?}", metadata);
+                        remove_metadata(metadata, &conn)?;
+                    }
 
-                Result::<_>::Ok(())
-            })?;
+                    Result::<_>::Ok(())
+                })?;
         }
 
         Ok(())
     });
 }
 
-fn remove_metadata(
-    metadata_address: String,
-    conn: &db::Connection,
-    client: &RpcClient,
-) -> Result<()> {
+fn remove_metadata(metadata_address: String, conn: &db::Connection) -> Result<()> {
     let now = Local::now().naive_utc();
-    let slot = i64::try_from(client.get_slot().context("failed to get slot")?)?;
 
     update(metadatas::table.filter(metadatas::address.eq(metadata_address)))
-        .set((metadatas::burned_at.eq(now), metadatas::slot.eq(slot)))
+        .set(metadatas::burned_at.eq(now))
         .execute(conn)
         .context("couldnt set burned_at")?;
 
     Ok(())
 }
 
-fn account_exists(metadata_address: Pubkey, client: &RpcClient) -> Result<bool> {
-    let result = client.get_account(&metadata_address);
+fn account_exists(token_account: Pubkey, client: &RpcClient) -> Result<bool> {
+    let result = client.get_account(&token_account);
     match result {
         Err(ClientError {
             request: _,
