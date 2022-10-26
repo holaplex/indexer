da4dc610fd3291e110e01749ff50d13c2820af64
Abdul Basit <baronbasit@gmail.com>
2022-10-27 00:58:25 +0500

    ;;

--- crates/core/migrations/2022-10-26-193028_listings_indexes_and_metadatas_burned_at_is_null_idx/up.sql
+++ crates/core/migrations/2022-10-26-193028_listings_indexes_and_metadatas_burned_at_is_null_idx/up.sql
@@ -3,5 +3,5 @@ CREATE INDEX IF NOT EXISTS listings_canceled_at_is_null_idx ON public.listings U
 CREATE INDEX IF NOT EXISTS metadatas_burned_at_is_null_idx ON public.metadatas USING btree (burned_at) WHERE burned_at is null;
 CREATE INDEX IF NOT EXISTS listings_created_at_idx ON public.listings USING btree (created_at);
 CREATE INDEX IF NOT EXISTS listings_expiry_is_null_idx ON public.listings USING btree (expiry) WHERE expiry is null;
-CREATE INDEX IF NOT EXISTS listings_created_at_desc_idx ON public.listings USING btree (created_at DESC)
-CREATE INDEX IF NOT EXISTS listings_created_at_asc_idx ON public.listings USING btree (created_at ASC)
+CREATE INDEX IF NOT EXISTS listings_created_at_desc_idx ON public.listings USING btree (created_at DESC);
+CREATE INDEX IF NOT EXISTS listings_created_at_asc_idx ON public.listings USING btree (created_at ASC);
