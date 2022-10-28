642f67ab9ececcdfd6e1a570c68247e8758e04c5
Abdul Basit <baronbasit@gmail.com>
2022-10-28 20:58:54 +0500

    index instructions in separate run block

--- crates/indexer/src/geyser/instructions/hpl_reward_center/close_listing.rs
+++ crates/indexer/src/geyser/instructions/hpl_reward_center/close_listing.rs
@@ -27,7 +27,8 @@ pub(crate) async fn process(
             let token_size = rewards_listings::table
                 .select(rewards_listings::token_size)
                 .filter(rewards_listings::address.eq(accts[1].clone()))
-                .first(db)?;
+                .first(db)
+                .optional()?;
 
             let row = HplRewardCenterCloseListing {
                 wallet: Owned(accts[0].clone()),
@@ -41,15 +42,22 @@ pub(crate) async fn process(
                 auction_house_fee_account: Owned(accts[8].clone()),
                 trade_state: Owned(accts[9].clone()),
                 ah_auctioneer_pda: Owned(accts[10].clone()),
-                token_size,
+                token_size: token_size.unwrap_or_default(),
                 created_at: Utc::now().naive_utc(),
                 slot,
             };
-            db.build_transaction().read_write().run(|| {
-                insert_into(hpl_reward_center_close_listing_ins::table)
-                    .values(&row)
-                    .execute(db)?;
 
+            insert_into(hpl_reward_center_close_listing_ins::table)
+                .values(&row)
+                .execute(db)
+        })
+        .await
+        .context("failed to insert reward center close listing instruction ")?;
+
+    client
+        .db()
+        .run(move |db| {
+            db.build_transaction().read_write().run(|| {
                 update(
                     rewards_listings::table.filter(rewards_listings::address.eq(listing_address)),
                 )
