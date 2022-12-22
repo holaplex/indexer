f0397176fda5147dcc6c9f6919ca7fe5ac4e0208
raykast <me@ryan-s.net>
2022-12-21 18:03:30 -0800

    Remove commented dead code.

--- crates/indexer/src/util.rs
+++ crates/indexer/src/util.rs
@@ -2,8 +2,6 @@
 #![allow(dead_code)]
 
 use indexer_core::prelude::*;
-// #[cfg(feature = "mpl-token-metadata")]
-// use mpl_token_metadata::state::{Key, MasterEditionV1, MasterEditionV2};
 use solana_program::{account_info::AccountInfo, pubkey::Pubkey};
 
 /// Borrow an account's raw as a `solana-program` account info struct.
@@ -42,69 +40,3 @@ pub async fn account_data_as_info<T: Send + 'static>(
     .await
     .context("Failed to spawn dedicated thread for AccountInfo processing")
 }
-
-// /// Convenience wrapper for Metaplex's [`MasterEdition`] trait and structs
-// #[derive(Debug)]
-// #[cfg(feature = "mpl-token-metadata")]
-// pub enum MasterEdition {
-//     /// A v1 master edition
-//     V1(MasterEditionV1),
-//     /// A v2 master edition
-//     V2(MasterEditionV2),
-// }
-
-// #[cfg(feature = "mpl-token-metadata")]
-// impl MasterEdition {
-//     /// Construct the correct master edition from an account
-//     ///
-//     /// # Errors
-//     /// This function fails if the account cannot be parsed as a v1 account or a v2 account.
-//     pub fn from_account_info(
-//         info: &AccountInfo,
-//     ) -> Result<Self, solana_program::program_error::ProgramError> {
-//         MasterEditionV2::from_account_info(info)
-//             .map(Self::V2)
-//             .or_else(|e| {
-//                 debug!("Couldn't parse MasterEditionV2: {:?}", e);
-//                 MasterEditionV1::from_account_info(info).map(Self::V1)
-//             })
-//     }
-// }
-
-// #[cfg(feature = "mpl-token-metadata")]
-// impl mpl_token_metadata::state::MasterEdition for MasterEdition {
-//     fn key(&self) -> Key {
-//         match self {
-//             Self::V1(m) => m.key(),
-//             Self::V2(m) => m.key(),
-//         }
-//     }
-
-//     fn supply(&self) -> u64 {
-//         match self {
-//             Self::V1(m) => m.supply(),
-//             Self::V2(m) => m.supply(),
-//         }
-//     }
-
-//     fn set_supply(&mut self, supply: u64) {
-//         match self {
-//             Self::V1(m) => m.set_supply(supply),
-//             Self::V2(m) => m.set_supply(supply),
-//         }
-//     }
-
-//     fn max_supply(&self) -> Option<u64> {
-//         match self {
-//             Self::V1(m) => m.max_supply(),
-//             Self::V2(m) => m.max_supply(),
-//         }
-//     }
-
-//     fn save(&self, account: &AccountInfo) -> solana_program::entrypoint::ProgramResult {
-//         match self {
-//             Self::V1(m) => m.save(account),
-//             Self::V2(m) => m.save(account),
-//         }
-//     }
-// }
