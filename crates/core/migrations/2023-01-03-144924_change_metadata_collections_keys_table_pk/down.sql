eb543559cd48068cc5892bfc94ba69c49c92bdc3
Abdul Basit <baronbasit@gmail.com>
2023-01-03 21:31:03 +0500

    Make metadata_address PK in metadata_collection_keys table

---
+++ crates/core/migrations/2023-01-03-144924_change_metadata_collections_keys_table_pk/down.sql
@@ -0,0 +1,17 @@
+DO $$
+BEGIN
+IF EXISTS ( 
+	SELECT conrelid::regclass AS table_name, 
+		   conname AS primary_key, 
+		   pg_get_constraintdef(oid) 
+	FROM   pg_constraint 
+	WHERE  contype = 'p' 
+	AND    connamespace = 'public'::regnamespace  
+	AND    conrelid::regclass::text = 'metadata_collection_keys'
+	and pg_get_constraintdef(oid) = 'PRIMARY KEY (metadata_address)'
+	ORDER  BY conrelid::regclass::text, contype DESC
+) F THEN
+ALTER TABLE metadata_collection_keys DROP CONSTRAINT metadata_collection_keys_pkey;
+ALTER TABLE metadata_collection_keys ADD PRIMARY KEY (metadata_address);
+END IF;
+END $$;
